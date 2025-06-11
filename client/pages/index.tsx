f54469def9b393ba8094af0e29a48ea62c5bcd47
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-10 21:08:38 -0700

    finished useEffect and getting data

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React, { use, useEffect, useState } from "react";
 
 function index() {
-  return (
-    <div>
-      <h1>Welcome to The Treasure Trove!</h1>
-    </div>
-  );
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+      .then((response) => response.json())
+      .then((data) => 
+        //this has to be a string: data.message otherise it won't render properly
+        setMessage(data.message || "No message received"));
+  }, []);
+
+  return <div>{message}</div>;
 }
 
 export default index;
