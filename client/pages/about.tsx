67bad7d713a68dc5ec1e638ea2ad501d9c079c31
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-14 20:21:11 -0700

    created about page

---
+++ client/pages/about.tsx
@@ -0,0 +1,11 @@
+const about = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam adipisci nisi quam culpa alias voluptates ipsam. Nisi doloribus optio omnis, distinctio eum asperiores dolor quasi nobis culpa cumque blanditiis. Optio.</p>
+     
+        </>
+    );   
+}
+ 
+export default about;
