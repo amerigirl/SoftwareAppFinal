828fa8b9faf5f475370d4c4240ceeb8c10347776
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-16 10:47:59 -0700

    updated about page with welcome and image

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -3,18 +3,51 @@ import { Box, Typography } from "@mui/material";
 const about = () => {
   return (
     <>
-          <Box sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center", 
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "3rem",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: "x-large",
+          }}
+        >
+          Welcome to The Treasure Trove!
+        </Typography>
+      </Box>
+      <Box>
+        <Box  
+          sx={{
+            display: "flex"
+            
+          }}
+        >
+          <Typography
+            sx={{
+                
+              marginLeft: "5rem",
               marginTop: "3rem",
-          }}>
-              <Typography sx={{
-                  fontWeight: "bold", 
-                  fontSize: "x-large"
-              }}>
-                  Welcome to The Treasure Trove!
-              </Typography>
+            }}
+          >
+            The Treasure Trove is your one stop shop for all the best in movie
+            entertainment!
+          </Typography>
+        </Box>
+        <Box
+          component="img"
+          src="/movieReel.avif"
+          alt="Description"
+          sx={{
+            width: 300,
+            height: "auto",
+            borderRadius: 2,
+          }}
+        />
       </Box>
     </>
   );
