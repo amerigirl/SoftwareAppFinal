f54c7a8c2cf3f5c2581191e3bb50f198c1cadd7a
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-16 11:23:49 -0700

    troubleshooting the about page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,4 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card, Button } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
 
 const about = () => {
   return (
@@ -8,46 +12,86 @@ const about = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          marginTop: "3rem",
+          marginTop: "5rem",
         }}
       >
         <Typography
           sx={{
             fontWeight: "bold",
-            fontSize: "x-large",
+            fontSize: "47px",
           }}
         >
           Welcome to The Treasure Trove!
         </Typography>
       </Box>
-      <Box>
-        <Box  
-          sx={{
-            display: "flex"
-            
-          }}
-        >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "3rem",
+          gap: "5rem",
+        }}
+      >
+        <Box>
           <Typography
             sx={{
-                
               marginLeft: "5rem",
               marginTop: "3rem",
+              fontSize: "20px",
+              whiteSpace: "normal",
+              textIndent: "2rem",
             }}
           >
             The Treasure Trove is your one stop shop for all the best in movie
-            entertainment!
+            entertainment. Please take a moment to browse through our amazing
+            catalog! We invite you to browse, checkout and even add suggestions
+            to this curated list of goodies :)
           </Typography>
         </Box>
         <Box
           component="img"
           src="/movieReel.avif"
-          alt="Description"
+          alt="Movie Reel"
           sx={{
-            width: 300,
+            width: 350,
             height: "auto",
-            borderRadius: 2,
+            marginTop: "-4rem",
           }}
         />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly", // or "space-between" or "center"
+          alignItems: "flex-start", // aligns cards at the top
+          flexWrap: "wrap", // allows wrapping on smaller screens
+          width: "50%",
+          minHeight: 500,
+        }}
+      >
+        <Card
+          sx={{
+            maxWidth: 345,
+          }}
+        >
+          <CardMedia
+            sx={{ height: 450 }}
+            image="/client/pages/movieReel.avif"
+            title="My Best Friend's Wedding"
+          />
+        </Card>
+        <Card
+          sx={{
+            maxWidth: 345,
+          }}
+        >
+          <CardMedia
+            sx={{ height: 450 }}
+            image="/movieReel.avif"
+            title="My Best Friend's Wedding"
+          />
+        </Card>
       </Box>
     </>
   );
