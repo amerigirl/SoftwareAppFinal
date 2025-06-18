94a1b396cc5ee300ba7c1362bbb38f71f74f760e
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-17 20:39:25 -0700

    working with images and page structure

--- client/pages/searchpage.tsx
+++ client/pages/searchpage.tsx
@@ -1,43 +1,51 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const searchpage = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: "5rem",
-        gap: "2rem",
-      }}
-    >
-      <Typography sx={{ fontSize: "1.5rem" }}>
-        Your movie awaits! Start typing in the search bar to get started!
-      </Typography>
-
+    <>
       <Box
-        component="img"
-        src="/fantasyMovies.avif"
-        alt="random movie poster"
         sx={{
-          width: 700,
-          height: 300,
-          objectFit: "cover",
+          display: "flex",
+          gap: "2.5rem",
+          marginTop: "5rem",
+          justifyContent: "center",
         }}
-      />
-    </Box>
-       <Box
-       component="img"
-       src="/madMax.webp"
-       alt="mad max"
-       sx={{
-         width: 700,
-         height: 300,
-         objectFit: "cover",
-       }}
-     />
-   </Box>
-  );
-}
+      >
+        <Typography sx={{ fontSize: "1.5rem" }}>
+          Your movie awaits! Start typing in the search bar to get started!
+        </Typography>
+        <Box
+          sx={{
+            display: "grid",
+            gap: "2rem",
+          }}
+        >
+          <Box
+            component="img"
+            src="/fantasyMovies.avif"
+            alt="random movie poster"
+            sx={{
+              width: 700,
+              height: 300,
+              border: "3px solid black",
+            }}
+          />
 
-export default searchpage
+          <Box
+            component="img"
+            src="/madMax.webp"
+            alt="mad max"
+            sx={{
+              width: 700,
+              height: 300,
+              border: "3px solid black",
+            }}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default searchpage;
