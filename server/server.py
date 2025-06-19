dd6d1a421482cdd4e6e8cc149cd39dd58585add4
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-18 18:02:17 -0700

    added models file; added to server.py(imports)

--- server/server.py
+++ server/server.py
@@ -1,10 +1,20 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from  flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db
+#from resources import 
 
 
 #app instance
 app = Flask(__name__)
 CORS(app)
+
+#connect to a DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 # create a route
 
