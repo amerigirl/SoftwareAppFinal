f641a2a5a14d83bf328ea0d2e73785becb84e2d8
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-19 07:23:27 -0700

    troubleshooting search issue

--- client/pages/searchpage.tsx
+++ client/pages/searchpage.tsx
@@ -1,14 +1,29 @@
 
+"use client"
+
 import React from "react";
+import { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
 
-const searchpage = () => {
+const SearchPage = () => {
+const [movieTitles, setMovieTitles] = useState([])
+const [isLoading, setIsLoading] = useState(true)
 
-
-
+  
+  useEffect(() => { 
+    fetch(" http://127.0.0.1:8080/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        //get titles
+        const titles = data.map((movie) => movie.title);
+        setMovieTitles(titles);
+        setIsLoading(false);
+      }, []);
+  })
+  
   return (
     <>
       <Box
@@ -54,17 +69,12 @@ const searchpage = () => {
         </Box>
       </Box>
       <Stack spacing={2} sx={{ width: 300 }}>
-        <Autocomplete
-          id="free-solo-demo"
-          freeSolo
-          options={top100Films.map((option) => option.title)}
-          renderInput={(params) => <TextField {...params} label="freeSolo" />}
-        />
+     
         <Autocomplete
           freeSolo
           id="free-solo-2-demo"
           disableClearable
-          options={top100Films.map((option) => option.title)}
+          options={movieTitles.map((option) => option.title)}
           renderInput={(params) => (
             <TextField
               {...params}
@@ -83,4 +93,4 @@ const searchpage = () => {
   );
 };
 
-export default searchpage;
+export default SearchPage;
