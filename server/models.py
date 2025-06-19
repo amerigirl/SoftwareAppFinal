347261a26728d59e806e50359d719405126d9e7c
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-19 16:55:24 -0700

    added a model for suggesting a movie

--- server/models.py
+++ server/models.py
@@ -9,6 +9,19 @@ class MovieModel(db.Model):
     genre = db.Column(db.String(50))
     #addimageurl
 
-#lets us see the object when we print it out
+    #lets us see the object when we print it out
     def __repr__(self):
         return f"<Movie {self.id} {self.title} {self.year} {self.genre}"
+
+
+class SuggestAMovieModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+    genre = db.Column(db.String(50))
+    comment = db.Column(db.String(500))
+
+
+#lets us see the object when we print it out
+    def __repr__(self):
+        return f"<Movie {self.id} {self.title} {self.year} {self.genre} {self.comment}"
