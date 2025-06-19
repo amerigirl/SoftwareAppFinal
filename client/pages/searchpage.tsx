4c09b660d246eb68fb7d613e4347d70da158ca5d
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-19 07:05:26 -0700

    added material ui framework for searchbar

--- client/pages/searchpage.tsx
+++ client/pages/searchpage.tsx
@@ -1,18 +1,25 @@
+
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const searchpage = () => {
+
+
+
   return (
     <>
       <Box
         sx={{
           display: "flex",
           gap: "2.5rem",
-          marginTop: "5rem",
+          marginTop: "10rem",
           justifyContent: "center",
         }}
       >
-        <Typography sx={{ fontSize: "1.5rem" }}>
+        <Typography sx={{ fontSize: "1.5rem", marginTop: "4rem" }}>
           Your movie awaits! Start typing in the search bar to get started!
         </Typography>
         <Box
@@ -26,9 +33,10 @@ const searchpage = () => {
             src="/fantasyMovies.avif"
             alt="random movie poster"
             sx={{
-              width: 700,
+              width: 600,
               height: 300,
               border: "3px solid black",
+              objectFit: "fill",
             }}
           />
 
@@ -37,13 +45,40 @@ const searchpage = () => {
             src="/madMax.webp"
             alt="mad max"
             sx={{
-              width: 700,
+              width: 600,
               height: 300,
               border: "3px solid black",
+              objectFit: "fill",
             }}
           />
         </Box>
       </Box>
+      <Stack spacing={2} sx={{ width: 300 }}>
+        <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          options={top100Films.map((option) => option.title)}
+          renderInput={(params) => <TextField {...params} label="freeSolo" />}
+        />
+        <Autocomplete
+          freeSolo
+          id="free-solo-2-demo"
+          disableClearable
+          options={top100Films.map((option) => option.title)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search input"
+              slotProps={{
+                input: {
+                  ...params.InputProps,
+                  type: "search",
+                },
+              }}
+            />
+          )}
+        />
+      </Stack>
     </>
   );
 };
